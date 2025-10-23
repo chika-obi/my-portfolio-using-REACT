f50e7807dc66b97a84512ded27a3c0144046ac03
Chika-Obi <kpanukuchikaobi@gmail.com>
2025-10-23 04:21:37 +0100

    Phone Responsiveness Updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./Hero.css";
-import banner from "../../assets/banner2.png"; // ✅ absolute import works with Vite
 
 const Hero = () => {
   return (
-    <section
-      className="hero"
-      id="home"
-      style={{ backgroundImage: `url(${banner})` }}
-    >
+    <section className="hero" id="home">
       <div className="hero-overlay">
         <div className="hero-content">
-          {/* <h1>
-            Hi, I’m <span>Chika Obi</span>
-          </h1>
-          <p>A Passionate Frontend Developer & Designer</p> */}
-          <br />  <br />  <br /><br />  <br /><br />  <br />
+          <br /> <br /> <br /> <br /> <br /> <br /> <br />
           <a href="#projects" className="hero-btn">
             View My Work
           </a>
